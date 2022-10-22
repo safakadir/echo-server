3b13f0b2ede726e409dbe52b45037149fe6e2029
safakadir <safakadir@gmail.com>
2022-10-22 06:57:27 +0300

    echo server implemented

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const provideResponse = (req) => {
+    return {
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+        path: req.path,
+        queryParams: req.queryParams
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello from the echo server!");
+})
+
+app.get('/get', (req, res) => {
+    res.json(provideResponse(req));
+});
+app.post('/get', (req, res) => {
+    res.json(provideResponse(req));
+});
+app.put('/get', (req, res) => {
+    res.json(provideResponse(req));
+});
+app.delete('/get', (req, res) => {
+    res.json(provideResponse(req));
+});
+
+app.listen(port, () => console.log(`Echo server listening on port ${port}!`));
